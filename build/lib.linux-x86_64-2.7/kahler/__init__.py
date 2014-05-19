22a03b6b6813ca5e9027c61a5af7f0c56eb5048e
aeftimia <alexeftimiades@gmail.com>
2014-05-19 08:26:48 -0400

    remove automatically generated files

--- build/lib.linux-x86_64-2.7/kahler/__init__.py
+++
@@ -1,6 +0,0 @@
-from parallel import *
-from grid_utils import *
-from form_utils import *
-from skeleton import *
-from simplicial_complex import *
-__all__ = filter(lambda s:not s.startswith('_'),dir())
